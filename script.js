1143c3b5586d8884d874f04e126b15edfba3941d
mao476 <39281533+mao476@users.noreply.github.com>
2019-11-22 23:34:33 +0400

    final edits on page 5 and 6

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ $("#submit").click(function()
     var $startaorp = $('#startaorp').val();
     var $endaorp = $('#endaorp').val();
 
-    console.log($name);
     $('#postsTable').append("<tr><td>" + $name + "</td><td>" + $contact + "</td><td style='text-align:center'>" + $mealswipes + "</td><td>" + $starttime + $startaorp + "-" + $endtime + $endaorp + "</td></tr>");
+ 
+    document.getElementById("myForm").reset();
+    document.scrollTop(0);
 })
+
+window.onload = function() {
+ setTimeout (function () {
+  scrollTo(0,0);
+ }, 100); //100ms for example
+}
+
