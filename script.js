0b3060b6a5bc74de9aaaf0e0359ce6811bfad451
mao476 <39281533+mao476@users.noreply.github.com>
2019-11-23 12:59:12 +0400

    all pages connected. Final file

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$("#submit").click(function()
+$("#submission").click(function()
 {
     var $name = $('#name').val();
     var $contact = $('#contact').val();
@@ -7,11 +7,38 @@ $("#submit").click(function()
     var $endtime = $('#endtime').val();
     var $startaorp = $('#startaorp').val();
     var $endaorp = $('#endaorp').val();
-
-    $('#postsTable').append("<tr><td>" + $name + "</td><td>" + $contact + "</td><td style='text-align:center'>" + $mealswipes + "</td><td>" + $starttime + $startaorp + "-" + $endtime + $endaorp + "</td></tr>");
- 
-    document.getElementById("myForm").reset();
-    document.scrollTop(0);
+    var start, end;
+    if($name && $contact && $mealswipes && $starttime && $endtime && $startaorp && $endaorp){
+        if($startaorp == "pm")
+            start = parseInt($starttime) + 12;
+        else{
+            start = parseInt($starttime);
+        }
+        
+        if($endaorp == "pm")
+            end = parseInt($endtime) + 12;
+        else{
+            end = parseInt($endtime);
+        }
+        console.log(start);
+        console.log(end);
+        console.log($startaorp);
+        console.log($endaorp);
+        if(start < end){
+            $('#postsTable').append("<tr><td>" + $name + "</td><td>" + $contact + "</td><td style='text-align:center'>" + $mealswipes + "</td><td>" + $starttime + $startaorp + "-" + $endtime + $endaorp + "</td></tr>");
+            document.getElementById("myForm").reset();
+            scrollTo(0,0);
+            document.getElementById("errorMessage").innerHTML=""
+        }
+        else{
+            document.getElementById("errorMessage").innerHTML="Timings are not valid."           
+        }
+    }
+    else{
+        console.log("Field not filled")
+        document.getElementById("errorMessage").innerHTML="Please fill all fields."   
+    }
+                    
 })
 
 window.onload = function() {
